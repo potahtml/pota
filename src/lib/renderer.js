f1fad9b6b0180fbbe8927e62985ef2606418f867
Tito Bouzout <tito.bouzout@gmail.com>
2023-07-12 13:43:29 -0300

    Reuse element as marker/placeholder

--- src/lib/renderer.js
+++ src/lib/renderer.js
@@ -12,22 +12,23 @@ export function createFragment(props, ...children) {
 
 export function createElement(tag, props, ...children) {
   // should return wrapped so the code runs from parent to child
-  return () => {
+  // return memos to avoid re-excuting the components
+  // example fn Component(){ return (<Show when={signal}></Show>) } will re-execute if not memoized
+  return createMemo(() => {
     // a component function
     if (typeof tag === 'function') {
-      return tag(props, children)
+      return createMemo(() => tag(props, children))
     }
 
     // a regular html tag
     console.log('creating tag', tag, props)
-
     const element = document.createElement(tag)
 
     // naive assign props to the tag
     if (props) {
       Object.entries(props).forEach(([name, value]) => {
         if (name === 'onMount') {
-          // not good but for the sake of testing
+          // TODO: not good but for the sake of testing
           element.onMount = value
         } else if (value === null || value === undefined) {
           element.removeAttribute(name)
@@ -48,56 +49,70 @@ export function createElement(tag, props, ...children) {
         } else if (name.startsWith('on') && name.toLowerCase() in window) {
           element.addEventListener(name.toLowerCase().substr(2), value)
         } else {
-          // not good but for the sake of testing
+          // TODO: not good but for the sake of testing
           element[name] = value
           element.setAttribute(name, value)
         }
       })
     }
+
     // insert children
     insertChildren(element, children)
 
     return element
+  })
+}
+
+// TODO: disposer should remove the added elements not just clean the parent
+export function render(children, parent) {
+  let disposer
+  createRoot(dispose => {
+    disposer = dispose
+    insertChildren(parent, children)
+  })
+  return () => {
+    disposer()
+    parent.textContent = ''
   }
 }
 
 export function insertChildren(parent, children) {
   // placeholder so elements stay in position
-  const placeholder = document.createComment('placeholder')
-  parent.appendChild(placeholder)
+  let marker = /*document.createTextNode('') || */ document.createComment('placeholder')
+  parent.appendChild(marker)
+  let placeholder = marker
+
+  onCleanup(() => {
+    parent.removeChild(placeholder)
+  })
 
   createEffect(() => {
+    // a children could be a component
     let child = unwrap(children)
 
     // 0 and empty text are valid childs
-    if (child !== undefined && child !== null) {
+    if (child !== null) {
       if (Array.isArray(child)) {
         child.forEach(child => insertChildren(parent, child))
       } else {
-        // console.log('rendering', child, child.outerHTML)
-
         // create text node if isnt a dom element
         const element = child && child.nodeType ? child : document.createTextNode(child)
 
         // put the node in place
-        parent.insertBefore(element, placeholder)
+        parent.replaceChild(element, placeholder)
+        placeholder = element
 
         // call onMount if defined
         child && child.onMount && child.onMount(element)
-
-        // remove the element from the tree on cleanup
-        onCleanup(() => {
-          if (element.isConnected) {
-            parent.removeChild(element)
-          }
-        })
       }
+    } else {
+      // value has been emptied
+      if (placeholder.isConnected) {
+        parent.replaceChild(marker, placeholder)
+      }
+      placeholder = marker
     }
   })
-}
-
-export function render(children, parent) {
-  insertChildren(parent, children)
 }
 
 // helpers
