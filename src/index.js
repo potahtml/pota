7fbc8d1d076168218cab4ac1a1efd165fe4eb1a8
Tito Bouzout <tito.bouzout@gmail.com>
2023-07-20 06:38:43 -0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let root, renderEffect, effect, cleanup, signal, memo, untrack
 
 // for being able to switch reactive libraries easily
+
 export function setReactiveLibrary(o) {
 	root = o.root
 	renderEffect = o.renderEffect
@@ -13,6 +14,8 @@ export function setReactiveLibrary(o) {
 
 // constants
 
+const $component = Symbol('component')
+
 // while not needed these make the logic/code more readable
 
 const assign = Object.assign
@@ -20,12 +23,27 @@ const entries = Object.entries
 
 const isArray = Array.isArray
 const isFunction = v => typeof v === 'function'
-const isComponent = v => v && v[$component] === null
-const isResolved = v => v && v[$resolved] === null
+const isComponentFunction = v => v && v[$component] === ComponentFunction
+const isComponentNode = v => v && v[$component] === ComponentNode
+const isComponentFragment = v => v && v.displayName === 'createFragment'
+const isComponent = v =>
+	isComponentNode(v) || isComponentFunction(v) || isComponentFragment(v)
+const isNode = v => v && v.nodeType // v instanceof Node
+const isDisplayable = v => {
+	const type = typeof v
+	return (
+		type === 'string' ||
+		type === 'number' ||
+		type === 'boolean' ||
+		v === undefined ||
+		type === 'bigint'
+	)
+}
 
 const getValue = v => (isFunction(v) ? v() : v)
 const call = (fns, ...args) => fns && fns.forEach(fn => fn(...args))
 
+// todo: allow to change document
 const createElement = document.createElement.bind(document)
 const createElementNS = document.createElementNS.bind(document)
 const createTextNode = document.createTextNode.bind(document)
@@ -41,21 +59,19 @@ const NSProps = {
 	'xlink:href': 'http://www.w3.org/1999/xlink',
 }
 
-const $component = Symbol('component')
-const $resolved = Symbol('resolved')
-
 // components
 
-function Component(tag, props, children, name, parent) {
-	// user defined component
-	if (isFunction(tag)) {
-		return untrack(() => tag(props, children))
-	}
-
-	// a html tag component
-	return untrack(() => createNode(tag, props, children, name, parent))
+// <MyComponent ../>
+function ComponentFunction(fn, props, children) {
+	return untrack(() => fn(props, children))
 }
 
+// <div ../>
+function ComponentNode(tagName, props, children, parent) {
+	return untrack(() => createNode(tagName, props, children, parent))
+}
+
+// <>...</>
 export function createFragment(props, children) {
 	return children
 }
@@ -63,30 +79,33 @@ export function createFragment(props, children) {
 // must return a function so we render from parent to children instead of from children to parent
 // this allows to access parent from children
 // having parent before children creation is helpful for example to create svgs and spread the namespace downwards
-export function createComponent(tag, props, ...children) {
-	props = props || Object.create(null)
-	const name = tag.name !== undefined ? tag.name : tag
 
-	return assign(Component.bind(null, tag, props, children, name), {
-		[$component]: null,
-		componentName: name,
-		tag,
+export function createComponent(unknown, props, ...children) {
+	props = props || Object.create(null)
+
+	const component = isFunction(unknown) ? ComponentFunction : ComponentNode
+
+	const meta = {
+		[$component]: component,
+		displayName: component === ComponentNode ? unknown : unknown.name || 'anon function',
+		unknown,
 		props,
 		children,
-	})
+	}
+	return assign(component.bind(meta, unknown, props, children), meta)
 }
 
 // rendering
 
-export function render(children, parent) {
+export function render(unknown, parent) {
 	return root(dispose => {
-		insertChildren(parent, createComponent(children))
+		insertChildren(parent, createComponent(unknown))
 		return dispose
 	})
 }
 
-// a x/html tag element
-export function createNode(tag, props, children, name, parent) {
+// a x/html element
+export function createNode(tag, props, children, parent) {
 	// resolve the namespace
 	const ns = props.xmlns
 		? props.xmlns // the prop contains the namespace
@@ -95,117 +114,146 @@ export function createNode(tag, props, children, name, parent) {
 		? parent.namespaceURI // the parent contains the namespace
 		: NS[tag] // special case svg, math in case of missing xmlns attribute
 
+	// todo handle real nodes coming in `tag`
 	const node = ns ? createElementNS(ns, tag) : createElement(tag)
+
+	// get rid of the node on cleanup
+	cleanup(() => node.remove())
 
 	// assign the props to the tag
 	entries(props).forEach(([name, value]) => {
 		assignProps(node, name, value)
 	})
 
-	// get rid of the node on cleanup
-	cleanup(() => {
-		console.log('deleting 1', node)
-		node.remove()
-	})
-
 	// insert childrens
-	if (children.length) {
-		// resolve in line the most common case of 1 children
-		insertChildren(node, children.length === 1 ? children[0] : children, null, name)
-	}
+	// resolve in line the most common case of 1 children
+	if (children.length)
+		insertChildren(node, children.length === 1 ? children[0] : children)
 
 	return node
 }
 
-function insertChildren(parent, child, placeholder, name) {
+// this function returns just to please the `For` component
+// it NEEDS to return a valid dom node
+function insertChildren(parent, child, placeholder) {
 	// a placeholder helps to keep nodes in position
-	// the placeholder is later replaced by the actual node
 
-	placeholder = !placeholder
-		? parent.appendChild(
-				createComment(
-					'parent doesnt provide placeholder - ' + name + ' - ' + typeof child,
-				),
-		  )
-		: // in the case of arrays, position nodes relative to the component
-		  parent.insertBefore(
-				createComment('placeholder from parent - ' + name + ' - ' + typeof child),
-				placeholder,
-		  )
+	function createPlaceholder(relative, text) {
+		placeholder =
+			!placeholder || !relative
+				? parent.appendChild(createComment(text || ''))
+				: // provided by parent
+				  parent.insertBefore(createComment('by parent - ' + text || ''), placeholder)
 
-	cleanup(() => {
-		placeholder.remove()
-	})
+		// get rid of the placeholder on cleanup
+		cleanup(() => placeholder.remove())
+	}
 
+	if (isDisplayable(child)) {
+		// undefined/string/number/boolean/bigint
+
+		// create a text node
+		const node = createTextNode(child)
+
+		// insert node
+		insertNode(parent, node, placeholder)
+
+		return node
+	}
+
+	// TODO: on here maybe use flat, maybe not
 	if (isArray(child)) {
-		// TODO: on here maybe use flat, maybe not
-		return child.map(child => insertChildren(parent, child, placeholder, name))
+		return child.map(child => insertChildren(parent, child, placeholder))
 	}
 
-	if (isComponent(child)) {
-		return insertChildren(parent, child(parent), placeholder, name)
+	// TODO: on here maybe use flat, maybe not
+	if (isComponentFragment(child)) {
+		return child().map(child => insertChildren(parent, child, placeholder))
 	}
 
+	// DOM Node
+	if (isNode(child)) {
+		const node = child
+
+		insertNode(parent, node, placeholder)
+
+		// callbacks
+		node.onCleanup && cleanup(() => call(node.onCleanup, node))
+		call(node.onMount, node)
+
+		return node
+	}
+
+	if (isComponentNode(child)) {
+		return insertChildren(parent, child(parent), placeholder)
+	}
+
+	if (isComponentFunction(child)) {
+		return insertChildren(parent, child(), placeholder)
+	}
+
+	// signal/memo/external/user provided function
+	// CAREFUL moving function here up or down, its just checking for function
 	if (isFunction(child)) {
-		effect(previous =>
-			// function call child() doesnt need `parent`, its an external function/signal
-			insertChildren(parent, child(), placeholder, name),
-		)
-		// TODO: this makes it so the reference will change when sorting the items
-		return placeholder
+		// needs placeholder to stay in position OK
+		// needs `true` to stay in a relative position
+		createPlaceholder(true, child.displayName || child.name)
+
+		// maybe signal: needs an effect
+		const node = renderEffect(() => insertChildren(parent, child(), placeholder))
+
+		// if we return undefined it crash, need to return the placeholder
+		// its important to return a placeholder here
+		return node || placeholder
 	}
 
 	if (child === null) {
-		return placeholder // the value is null, as in {null}
+		// we are cheating here and not returning a placeholder
+		return createTextNode('') // the value is null, as in {null}
 	}
 
 	if (child instanceof MapArray) {
-		// `For`, the callback function will run only for new childs
-		return child.map(parent, child => insertChildren(parent, child, placeholder, name))
+		// needs `true` to stay in a relative position
+		createPlaceholder(true, 'MapArray')
+
+		// signal: needs an effect
+		return renderEffect(() =>
+			// `For`, the callback function will run only for new childs
+			// parent is needed to resolve the childs, or maybe not TODO
+			child.map(parent, child => insertChildren(parent, child, placeholder)),
+		)
 	}
 
-	// TODO: move this up
+	// object/symbol/catch all
 
-	// create a text node if isnt a node
-	const node = child.nodeType ? child : createTextNode(child)
-	markResolved(node)
+	// create a text node
+	// toString() is needed for symbols and other fancy objects
+	const node = createTextNode(child.toString())
 
-	// put the node in place by replacing the placeholder or the old node
-	placeholder.replaceWith(node)
-
-	// save the new node as the new placeholder (to be replaced if the node changes)
-	placeholder = node
-
-	// callbacks
-	if (child) {
-		child.onCleanup && cleanup(() => call(child.onCleanup, node))
-		call(child.onMount, node)
-	}
+	// insert node
+	insertNode(parent, node, placeholder)
 
 	return node
 }
 
-// this needs to resolve for real all of them
+function insertNode(parent, node, relativeTo) {
+	relativeTo ? parent.insertBefore(node, relativeTo) : parent.appendChild(node)
+	cleanup(() => node.remove())
+}
+
+// recursively resolve all children and return direct children
+
 function resolve(children, parent) {
-	if (children === null) {
-		return children // children is null as in {null}
-	}
-	if (isResolved(children)) {
-		return children
-	}
-	if (isComponent(children)) {
-		return children(parent)
+	if (isFunction(children)) {
+		return resolve(children(parent), parent)
 	}
 	if (isArray(children)) {
-		// it flats what createNode returns not the full tree
-		// so all of the direct children of a given 1 node
-		return (
-			children
-				.flat(Infinity)
-				// only resolve not resolved nodes
-				.map(child => (isResolved(child) ? child : resolve(child, parent)))
-				.flat(Infinity)
-		) // resolve may put more arrays into it
+		const childrens = []
+		for (let child of children) {
+			child = resolve(child, parent)
+			isArray(child) ? childrens.push.apply(childrens, child) : childrens.push(child)
+		}
+		return childrens
 	}
 	return children
 }
@@ -213,20 +261,23 @@ function resolve(children, parent) {
 // helper for making untracked callbacks
 
 function makeCallback(children) {
-	if (children.length === 1 && isFunction(children[0])) {
-		return markComponent(children[0])
+	if (children.length === 1) {
+		const cb = children[0]
+		if (isComponent(cb)) {
+			return cb // already untracking
+		}
+		if (isFunction(cb)) {
+			return markComponent(cb) // should untrack
+		}
+		return () => cb // simple value
 	}
 	return markComponent(() => children)
 }
 
 function markComponent(fn) {
 	return assign((...args) => untrack(() => fn(...args)), {
-		[$component]: true,
+		[$component]: ComponentFunction,
 	})
-}
-
-function markResolved(node) {
-	node[$resolved] = null
 }
 
 // control flow
@@ -261,9 +312,7 @@ class MapArray {
 		)
 
 		// order of nodes may have changed, reorder it
-
 		if (nodes.length > 1) {
-			// console.log(nodes, nodes[0].parentNode)
 			const parent = nodes[0].parentNode
 			for (let i = nodes.length - 1; i > 0; i--) {
 				const node = nodes[i]
@@ -302,7 +351,7 @@ function mapArray(list, cb) {
 		}))
 	}
 
-	return fn => {
+	return function mapper(fn) {
 		runId++
 
 		const items = list() || []
