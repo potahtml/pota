65ac67a369995cc00fa4ed3f4887eddb29acf8d6
Tito Bouzout <tito.bouzout@gmail.com>
2023-08-13 19:54:50 -0300

    Fix dynamic not running after the first time

--- src/components/controlFlow/Dynamic.js
+++ src/components/controlFlow/Dynamic.js
@@ -2,8 +2,6 @@
 import { create } from '#main'
 
 export function Dynamic(props) {
-	const component = props.component
 	// needs to be deleted else it will end in the tag as an attribute
-	props.component = null
-	return create(component)(props)
+	return create(props.component)({ ...props, component: null })
 }
