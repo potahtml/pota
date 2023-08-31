b804c244bdc31b2b2b7cde8d09f68213c98a8971
Tito Bouzout <tito.bouzout@gmail.com>
2023-08-31 16:55:17 -0300

    Improve performance of selector

--- src/lib/reactivity/selector.js
+++ src/lib/reactivity/selector.js
@@ -1,6 +1,32 @@
-import { memo } from '#main'
-import { getValue } from '#std'
+import { effect, signal } from '#main'
 
-export function selector(signal) {
-	return item => memo(() => getValue(item) === signal())
+export function selector(value) {
+	const map = new Map()
+
+	let prev
+	effect(() => {
+		const previous = map.get(prev)
+		if (previous) previous.write(false)
+
+		const selected = value()
+
+		const current = map.get(selected)
+		if (current) current.write(true)
+
+		prev = selected
+	})
+
+	const defaultValue = value()
+
+	return item => {
+		if (!map.has(item)) {
+			const [read, write] = signal(item === defaultValue)
+			map.set(item, {
+				read,
+				write,
+			})
+		}
+
+		return map.get(item).read
+	}
 }
