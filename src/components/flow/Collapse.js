392350e6408fa7a3d8e4bf09a2ac525dc4a50fc9
Tito Bouzout <tito.bouzout@gmail.com>
2023-10-12 05:51:17 -0300

    Make collapse component idiomatic and smaller

--- src/components/flow/Collapse.js
+++ src/components/flow/Collapse.js
@@ -1,36 +1,29 @@
-import { create, effect } from '#main'
-import { getValue } from '#std'
-
 // as Show but doesnt remove the children from the DOM
 
-/*customElements.define(
+customElements.define(
 	'pota-collapse',
-	class extends HTMLElement {
+	class collapse extends HTMLElement {
 		constructor() {
-			super(), this.attachShadow({ mode: 'open' }), this.hide()
+			super(), this.attachShadow({ mode: 'open' })
 		}
 		hide() {
-			this.shadowRoot.innerHTML = `<template><slot/></template>`
+			this.shadowRoot.innerHTML = ''
 		}
 		show() {
-			this.shadowRoot.innerHTML = `<slot/>`
+			this.shadowRoot.innerHTML = '<slot/>'
 		}
-		attributeChangedCallback(name, oldValue, newValue) {
-			newValue === 'true' ? this.show() : this.hide()
-		}
-		static get observedAttributes() {
-			return ['when']
+
+		set when(value) {
+			value ? this.show() : this.hide()
 		}
 	},
 )
 
-const WebComponent = create('pota-collapse')
-*/
 export function Collapse(props) {
-	const element = WebComponent({ children: props.children })
-
-	effect(() => {
-		element.setAttribute('when', getValue(props.when))
-	})
-	return element
+	return (
+		<pota-collapse
+			when={props.when}
+			children={props.children}
+		/>
+	)
 }
