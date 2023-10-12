0680e734463dbb8ba5234e6f935319ae7906d6c2
Tito Bouzout <tito.bouzout@gmail.com>
2023-10-12 06:03:28 -0300

    spacing

--- src/components/flow/Show.js
+++ src/components/flow/Show.js
@@ -8,10 +8,12 @@ export function Show(props) {
 	const callback = makeCallback(props.children)
 	const value = memo(() => getValue(props.when))
 	const condition = memo(() => !!value())
+
 	// needs resolve to avoid re-rendering
 	const fallback = hasValue(props.fallback)
 		? lazyMemo(() => resolve(props.fallback))
 		: () => null
+
 	return memo(() => {
 		const result = condition()
 		return result ? callback(value) : fallback
