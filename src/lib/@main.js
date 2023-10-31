6eccb25807f8a6063418edd56fb3b407a4744d5e
Tito Bouzout <tito.bouzout@gmail.com>
2023-10-31 08:14:20 -0300

    Export lib

---
+++ src/lib/@main.js
@@ -0,0 +1,12 @@
+// LIB
+
+// comp
+export { makeCallback } from './comp/makeCallback.js'
+
+// reactivity
+export { isReactive } from './reactivity/isReactive.js'
+
+// std
+export { getValue } from './std/getValue.js'
+export { optional } from './std/optional.js'
+export { removeFromArray } from './std/removeFromArray.js'
