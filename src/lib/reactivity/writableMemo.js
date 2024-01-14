12df2765a09b9a591ef749713988101bec730832
Tito Bouzout <tito.bouzout@gmail.com>
2024-01-13 23:54:01 -0300

    update writable memo with code provided by ryan

--- src/lib/reactivity/writableMemo.js
+++ src/lib/reactivity/writableMemo.js
@@ -1,41 +1,18 @@
 import { markReactive } from './markReactive.js'
-import {
-  cleanup,
-  effect,
-  memo,
-  signal,
-  untrack,
-} from './primitives/solid.js'
+import { memo, signal } from './primitives/solid.js'
 
 /**
  * Lazy and writable version of `memo`, its writable and will run the
  * function only when used
  *
+ * @author ryansolid
  * @param {Function} fn - Function to re-run when dependencies change
  * @returns {Signal}
  */
 export function writableMemo(fn) {
-  const [sleeping, setSleeping] = signal(true)
-
-  const [value, setValue] = signal()
-
-  const memoValue = memo(() => {
-    if (sleeping()) return untrack(value)
-    return fn()
-  })
-
-  effect(() => {
-    if (sleeping()) return
-    setValue(memoValue())
-  })
-
-  let read = () => {
-    setSleeping(false)
-    read = value
-    return value()
-  }
+  const result = memo(() => signal(fn()))
 
   return markReactive((...args) => {
-    return args.length ? setValue(args[0]) : read()
+    return args.length ? result()[1](args[0]) : result()[0]()
   })
 }
