7ac214b7949e1bd1fe8b9c228c80b363608bf373
Tito Bouzout <tito.bouzout@gmail.com>
2024-01-14 04:49:41 -0300

    mark argument as optional

--- src/lib/reactivity/functionSignal.js
+++ src/lib/reactivity/functionSignal.js
@@ -5,7 +5,7 @@ import { signal } from './primitives/solid.js'
  * A self contained signal function, when an argument is present it
  * writes to the signal, when theres no argument it reads the signal.
  *
- * @param {any} value? - Optional initial value
+ * @param {any} [value] - Optional initial value
  * @returns {((...args) => any) | (() => any)}
  */
 export function functionSignal(value) {
