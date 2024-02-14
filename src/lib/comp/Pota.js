8da115229f7483589daf2255b25491e915cdef47
Tito Bouzout <tito.bouzout@gmail.com>
2024-02-13 22:07:24 -0300

    update references to deprecated apis

--- src/lib/comp/Pota.js
+++ src/lib/comp/Pota.js
@@ -2,7 +2,7 @@ import { $class } from '../../constants.js'
 
 /**
  * Extend `Pota` and define a `render(props){}` method to create a
- * class component. `onReady(cb)` and `onCleanup(cb)` methods will be
+ * class component. `ready(cb)` and `cleanup(cb)` methods will be
  * registered automatically
  *
  * @url https://pota.quack.uy/Classes
