609b52e520e8a6c4645d6014acfbb975498c52a6
Anthony K. <kilospade@gmail.com>
2024-02-16 18:53:43 -0600

    remove stream checks

--- src/lib/streams/removeVideoTracks.js
+++ src/lib/streams/removeVideoTracks.js
@@ -4,9 +4,5 @@
  * @param {MediaStream} stream - The media stream from which to remove video tracks.
  */
 export function removeVideoTracks(stream) {
-	if (stream) {
-		stream
-			.getVideoTracks()
-			.forEach(track => stream.removeTrack(track))
-	}
+	stream.getVideoTracks().forEach(track => stream.removeTrack(track))
 }
