734e763d380721d6bf81770f474daf88386a71c2
Tito Bouzout <tito.bouzout@gmail.com>
2024-02-24 03:10:01 -0300

    make mutable compatible with classes. It wont change `functions` and will only set `setters/getters` on `own` properties

--- src/lib/reactivity/mutable.js
+++ src/lib/reactivity/mutable.js
@@ -1,8 +1,11 @@
+import { hasOwnProperty } from '../std/hasOwnProperty.js'
+import { isFunction } from '../std/isFunction.js'
 import { signal } from './primitives/solid.js'
 
 /**
  * Creates setters and getter signals for the immediate properties
- * that are already defined in the object. Non-recursive.
+ * that are already defined in the object. Non-recursive. It only
+ * affects own properties and doesnt affects functions.
  *
  * @template T
  * @param {GenericObject<T>} obj
@@ -10,11 +13,14 @@ import { signal } from './primitives/solid.js'
  */
 export function mutable(obj) {
 	for (const key in obj) {
-		const [get, set] = signal(obj[key])
-		Object.defineProperty(obj, key, {
-			get,
-			set,
-		})
+		const value = obj[key]
+		if (!isFunction(value) && hasOwnProperty(obj, key)) {
+			const [get, set] = signal(value)
+			Object.defineProperty(obj, key, {
+				get,
+				set,
+			})
+		}
 	}
 	return obj
 }
