90f12a6d439b9f1dc0e9dc7150ed89f0a9d2f376
Tito Bouzout <tito.bouzout@gmail.com>
2024-02-24 04:00:17 -0300

    export component library, individually and as a group for the standalone build

---
+++ src/components/library/@main.js
@@ -0,0 +1,10 @@
+import './@theme/animations.js'
+import './@theme/base.js'
+import './@theme/utility.js'
+// import './@theme/light.js'
+import './@theme/dark.js'
+
+import './@theme/tokens.js'
+
+// components
+import './alert/index.js'
