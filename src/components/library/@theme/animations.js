92dd5f0242871245382ca1b56298e872dec7396b
Tito Bouzout <tito.bouzout@gmail.com>
2024-02-24 03:55:12 -0300

    add animations css

---
+++ src/components/library/@theme/animations.js
@@ -0,0 +1,28 @@
+import { css } from '../../../lib/css/css.js'
+import { adoptedStyleSheets } from '../../../lib/dom/elements.js'
+
+export const animationsStyleSheet = css`
+	@keyframes scale-fade-in {
+		from {
+			opacity: 0;
+			scale: 0.8;
+		}
+		to {
+			opacity: 1;
+			scale: 1;
+		}
+	}
+
+	@keyframes scale-fade-out {
+		from {
+			opacity: 1;
+			scale: 1;
+		}
+		to {
+			opacity: 0;
+			scale: 0.8;
+		}
+	}
+`
+
+adoptedStyleSheets.push(animationsStyleSheet)
