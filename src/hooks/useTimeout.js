366a7ab24a57674a9c5c3038135756f2e3ede972
Tito Bouzout <tito.bouzout@gmail.com>
2024-02-25 07:43:05 -0300

    document `useTimeout`

--- src/hooks/useTimeout.js
+++ src/hooks/useTimeout.js
@@ -1,14 +1,23 @@
 import { cleanup } from '../lib/reactivity/primitives/solid.js'
 import { withValue } from '../lib/reactivity/withValue.js'
 
-export function useTimeout(callabck, timeout, ...args) {
+/**
+ * Creates a `setTimeout` that autodisposes. The `delay` could be
+ * reactive. The timeout is NOT started automatically.
+ *
+ * @param {Function} callback - Callback to run once delay completes
+ * @param {Signal | number} delay - Delay number or signal
+ * @param {any[]} args - Arguments to pass to the callback
+ * @returns {{ start: Function; stop: Function }}
+ */
+export function useTimeout(callback, delay, ...args) {
   let id
   const fn = {
     start: () => {
-      withValue(timeout, timeout => {
+      withValue(delay, delay => {
         fn.stop()
-        if (timeout < Infinity)
-          id = setTimeout(callabck, timeout, ...args)
+        if (delay < Infinity)
+          id = setTimeout(callback, delay, ...args)
       })
       return fn
     },
