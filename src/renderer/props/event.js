5e25c6ec924a2c710c77a5b86ed7a240c2c9cccd
Tito Bouzout <tito.bouzout@gmail.com>
2024-02-25 07:40:26 -0300

    make code smaller, remove comments referencing deprecated stuff

--- src/renderer/props/event.js
+++ src/renderer/props/event.js
@@ -1,7 +1,3 @@
-// events
-// delegated and native events are hold into an array property of the node
-// to avoid duplicated events that could be added by using `ns` in ease of organization
-
 import { cleanup } from '../../lib/reactivity/primitives/solid.js'
 import { empty } from '../../lib/std/empty.js'
 import { isFunction } from '../../lib/std/isFunction.js'
@@ -15,8 +11,7 @@ import { isFunction } from '../../lib/std/isFunction.js'
  * @param {string} ns
  */
 export const setEventNS = (node, name, value, props, localName, ns) =>
-	// delegated: no
-	addEventListener(node, localName, value, false)
+	addEventListener(node, localName, value)
 
 const EventNames = empty()
 
@@ -50,32 +45,23 @@ export function eventName(name) {
  * @param {string} type - The name of the event listener
  * @param {EventListenerOrEventListenerObject} handler - Function to
  *   handle the event
- * @param {boolean} [external] - External defaults to true and avoids
- *   returning an `off` function for the event listener
- * @returns {Function | void} - An `off` function for removing the
- *   event listener
+ * @returns {Function} - An `off` function for removing the event
+ *   listener
  * @url https://pota.quack.uy/props/EventListener
  */
-export function addEventListener(
-	node,
-	type,
-	handler,
-	external = true,
-) {
+export function addEventListener(node, type, handler) {
 	node.addEventListener(
 		type,
 		handler,
 		isFunction(handler) ? null : handler,
 	)
 
-	// remove event on cleanup
-	cleanup(() => {
-		removeEventListener(node, type, handler, false)
-	})
+	const off = () => removeEventListener(node, type, handler)
 
-	if (external) {
-		return () => removeEventListener(node, type, handler)
-	}
+	// remove event on cleanup
+	cleanup(off)
+
+	return off
 }
 
 /**
@@ -85,21 +71,12 @@ export function addEventListener(
  * @param {string} type - The name of the event listener
  * @param {EventListenerOrEventListenerObject} handler - Function to
  *   handle the event
- * @param {boolean} [external] - External defaults to true and avoids
- *   returning an `off` function for the event listener
- * @returns {Function | void} - An `on` function for adding back the
- *   event listener
+ * @returns {Function} - An `on` function for adding back the event
+ *   listener
  * @url https://pota.quack.uy/props/EventListener
  */
-export function removeEventListener(
-	node,
-	type,
-	handler,
-	external = true,
-) {
+export function removeEventListener(node, type, handler) {
 	node.removeEventListener(type, handler)
 
-	if (external) {
-		return () => addEventListener(node, type, handler)
-	}
+	return () => addEventListener(node, type, handler)
 }
