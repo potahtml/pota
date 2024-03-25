14f520c202d012a552c3c8fbd66705948bd6c4ad
Tito Bouzout <tito.bouzout@gmail.com>
2024-03-25 00:22:50 -0300

    document `isPrototypeProperty`

--- src/lib/std/isPrototypeProperty.js
+++ src/lib/std/isPrototypeProperty.js
@@ -1,6 +1,12 @@
 import { hasOwnProperty } from './hasOwnProperty.js'
 
-// returns true for keys that shouldnt be transformed into getters/setters
+/**
+ * Returns `true` if the property is defined in the prototype and
+ * absent in the object
+ *
+ * @param {{}} target
+ * @param {PropertyKey} key
+ */
 export const isPrototypeProperty = (target, key) =>
 	// must do `key in target` to check that it DOES have it somewhere
 	// must do !hasOwnProperty to check that isnt an own property
