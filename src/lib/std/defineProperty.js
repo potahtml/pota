2431c43c32f05be667a35710e77d59b428f48d46
Tito Bouzout <tito.bouzout@gmail.com>
2024-03-25 00:24:41 -0300

    improve `defineProperty` lib

--- src/lib/std/defineProperty.js
+++ src/lib/std/defineProperty.js
@@ -1,19 +1,43 @@
-const define = Object.defineProperty
+import { assign } from './assign.js'
+import { create } from './create.js'
 
-const defaults = {
-	enumerable: true,
-	configurable: true,
-}
+export const defineProperty = Object.defineProperty
 
 /**
- * Object.defineProperty
+ * Object.defineProperty with `enumerable` and `configurable` set to
+ * `true` unless overwriten by `descriptor` argument
  *
  * @param {object} target
  * @param {PropertyKey} key
  * @param {PropertyDescriptor} descriptor
  */
-export const defineProperty = (target, key, descriptor) =>
-	define(target, key, {
-		...defaults,
-		...descriptor,
-	})
+export const redefineProperty = (target, key, descriptor) =>
+	defineProperty(target, key, assign(create(defaults), descriptor))
+
+const defaults = {
+	__proto__: null,
+	configurable: true,
+	enumerable: true,
+}
+
+/**
+ * Object.defineProperty with `configurable`, `writable` and
+ * `enumerable` as `false`
+ *
+ * @param {object} target
+ * @param {PropertyKey} key
+ * @param {any} value
+ */
+export const definePropertyValueReadOnly = (target, key, value) => {
+	const descriptor = create(defaultsReadOnly)
+	descriptor.value = value
+	defineProperty(target, key, descriptor)
+}
+
+const defaultsReadOnly = {
+	__proto__: null,
+	configurable: false,
+	enumerable: false,
+	writable: false,
+	value: undefined,
+}
