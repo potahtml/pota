a3dcf9b8a1223df91e67d410103a6be663f495b3
Tito Bouzout <tito.bouzout@gmail.com>
2024-05-11 15:08:35 -0300

    change tab to spaces as its more readable for types

--- ts.pota.d.ts
+++ ts.pota.d.ts
@@ -1,26 +1,26 @@
 /** Signatures are added via JSDoc. */
 
 type Elements =
-	| ((HTMLElement | Element | Node) & EventTarget)
-	| null
-	| undefined
+  | ((HTMLElement | Element | Node) & EventTarget)
+  | null
+  | undefined
 
 // devTools
 
 type devToolsArguments =
-	| {
-			__dev?: {
-				__pota?: {
-					kind?: string
-					name?: string
+  | {
+      __dev?: {
+        __pota?: {
+          kind?: string
+          name?: string
 
-					file?: string
+          file?: string
 
-					value?: any
-				}
-			}
-	  }
-	| undefined
+          value?: any
+        }
+      }
+    }
+  | undefined
 
 type OwnerOptions = devToolsArguments
 
@@ -30,34 +30,34 @@ type Signal = () => any
 type SignalAccessor = () => any
 
 type SignalOptions =
-	| ({
-			equals?: false | ((a, b) => boolean)
-			label?: string
-	  } & devToolsArguments)
-	| undefined
+  | ({
+      equals?: false | ((a, b) => boolean)
+      label?: string
+    } & devToolsArguments)
+  | undefined
 
 type SignalSetter = (
-	value?: any | ((prevValue?: any) => any),
+  value?: any | ((prevValue?: any) => any),
 ) => unknown
 
 type SignalObject<T> =
-	| [Signal, SignalSetter, SignalSetter]
-	| { read: Signal; write: SignalSetter; update: SignalSetter }
+  | [Signal, SignalSetter, SignalSetter]
+  | { read: Signal; write: SignalSetter; update: SignalSetter }
 
 // props
 
 type Props = {
-	[key: PropertyKey]: any
+  [key: PropertyKey]: any
 }
 
 type When = Signal | boolean | any
 
 type Each =
-	| Signal
-	| (() => Each)
-	| unknown[]
-	| Map<unknown, unknown>
-	| Set<unknown>
+  | Signal
+  | (() => Each)
+  | unknown[]
+  | Map<unknown, unknown>
+  | Set<unknown>
 
 // components
 
@@ -66,16 +66,16 @@ type Children = any
 type Component = ((props?: Props) => Children) | Function
 
 type Componenteable =
-	| string
-	| Elements
-	| object
-	| FunctionConstructor
-	| Component
+  | string
+  | Elements
+  | object
+  | FunctionConstructor
+  | Component
 
 // objects
 
 type GenericObject<T> = {
-	[K in keyof T]: T[K]
+  [K in keyof T]: T[K]
 }
 
 type Generic<T> = T
@@ -83,10 +83,10 @@ type Generic<T> = T
 // tests
 
 type Expect = {
-	toBe: (expected: any) => Promise<any>
-	toHaveShape: (expected: any) => Promise<any>
-	not: {
-		toBe: (expected: any) => Promise<any>
-		toHaveShape: (expected: any) => Promise<any>
-	}
+  toBe: (expected: any) => Promise<any>
+  toHaveShape: (expected: any) => Promise<any>
+  not: {
+    toBe: (expected: any) => Promise<any>
+    toHaveShape: (expected: any) => Promise<any>
+  }
 }
