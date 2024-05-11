9b8b134d46e52ac4ec3990def81cce159a8eb60d
Tito Bouzout <tito.bouzout@gmail.com>
2024-05-11 14:33:17 -0300

    the transform add debugging arguments to functions named `signal`, on this helper whats named `signal` is an accessor, so rename it to `snigal` inspired by @bigmistqke with https://github.com/bigmistqke/snigal . Todo: should maybe merge this `proxy` functionality to `Signal` class.

--- src/lib/reactivity/proxy.js
+++ src/lib/reactivity/proxy.js
@@ -4,20 +4,20 @@ import { keys, nothing } from '../std/@main.js'
  * Proxies a signals property access so you dont have to call the
  * function
  *
- * @param {Signal} signal - Signal to proxy
+ * @param {Signal} snigal - Signal to proxy
  * @param {object} [target] - Target object for the proxy
  * @returns {object} An object that will read the properties from the
  *   signal
  */
-export const proxy = (signal, target = nothing) =>
+export const proxy = (snigal, target = nothing) =>
 	new Proxy(target, {
 		get(target, key) {
-			return signal()[key]
+			return snigal()[key]
 		},
 		has(target, key) {
-			return key in signal()
+			return key in snigal()
 		},
 		ownKeys(target) {
-			return keys(signal())
+			return keys(snigal())
 		},
 	})
