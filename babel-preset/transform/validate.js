b2eef41856ace206b5b4510f003eff48b56b69b5
Tito Bouzout <tito.bouzout@gmail.com>
2024-11-02 17:48:51 -0300

    update validate html code

--- babel-preset/transform/validate.js
+++ babel-preset/transform/validate.js
@@ -1,28 +1,19 @@
 import { error } from './utils.js'
 
-import * as parse5 from 'parse5'
+import { parse, parseFragment, serialize } from 'parse5'
 
-const bodyElement = parse5.parse(
+const bodyElement = parse(
 	`<!DOCTYPE html><html><head></head><body></body></html>`,
 	// @ts-ignore
 ).childNodes[1].childNodes[1]
 
 function innerHTML(htmlFragment) {
-	/**
-	 * Fragment will be parsed as if it was set to the `bodyElement`'s
-	 * `innerHTML` property.
-	 */
-	const parsedFragment = parse5.parseFragment(
-		bodyElement,
-		htmlFragment,
-	)
-
-	return parse5.serialize(parsedFragment)
+	return serialize(parseFragment(bodyElement, htmlFragment))
 }
 
 export function validatePartial(path, html) {
 	// partial validation
-	const clean = html
+	let clean = html
 		// remove content that isnt in between tags
 		.replace(/^[^<]+/, '#text')
 		.replace(/[^>]+$/, '#text')
@@ -34,33 +25,32 @@ export function validatePartial(path, html) {
 		.replace(/\s[a-z0-9-]+='[^']+'/gi, '')
 		.replace(/<([a-z0-9-:]+)\s+[^>]+>/gi, '<$1>')
 
-		// fix tables rows
-		.replace(/^<tr>/i, '<table><tbody><tr>')
-		.replace(/<\/tr>$/i, '</tr></tbody></table>')
-		// fix tables cells
-		.replace(/^<td>/i, '<table><tbody><tr><td>')
-		.replace(/<\/td>$/i, '</td></tr></tbody></table>')
-		.replace(/^<th>/i, '<table><thead><tr><th>')
-		.replace(/<\/th>$/i, '</th></tr></thead></table>')
-		// col/colgroup
-		.replace(/^<col>/i, '<table><colgroup><col>')
-		.replace(/<\/col>$/i, '</col></colgroup></table>')
-		.replace(/^<colgroup>/i, '<table><colgroup>')
-		.replace(/<\/colgroup>$/i, '</colgroup></table>')
-		// fix table components
-		.replace(/^<thead>/i, '<table><thead>')
-		.replace(/<\/thead>$/i, '</thead></table>')
-		.replace(/^<tbody>/i, '<table><tbody>')
-		.replace(/<\/tbody>$/i, '</tbody></table>')
-		.replace(/^<tfoot>/i, '<table><tfoot>')
-		.replace(/<\/tfoot>$/i, '</tfoot></table>')
+	// table cells
+	if (/^<(td|th)>/.test(clean)) {
+		clean = `<table><tbody><tr>${clean}</tr></tbody></table>`
+	}
 
-	const result = innerHTML(clean)
+	// table rows
+	if (/^<tr>/.test(clean)) {
+		clean = `<table><tbody>${clean}</tbody></table>`
+	}
 
-	if (result !== clean) {
+	// table misc
+	if (/^<col>/.test(clean)) {
+		clean = `<table><colgroup>${clean}</colgroup></table>`
+	}
+
+	// table components
+	if (/^<(thead|tbody|tfoot|colgroup|caption)>/.test(clean)) {
+		clean = `<table>${clean}</table>`
+	}
+
+	const browser = innerHTML(clean)
+
+	if (browser !== clean) {
 		console.warn('-'.repeat(80))
 		console.warn('User HTML:\n', clean)
-		console.warn('Browser HTML:\n', result)
+		console.warn('Browser HTML:\n', browser)
 		console.warn('Original HTML:\n', html)
 
 		error(
