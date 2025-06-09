0409c145ab584c18f3d45e85fb0e343cce600cca
Tito Bouzout <tito.bouzout@gmail.com>
2025-06-09 01:54:31 -0300

    add script to generate `importmap.json` and `types.json`, used in the docs site for live editing/docs/monaco

---
+++ src/release/generate.js
@@ -0,0 +1,111 @@
+// this script creates importmap.json and types.json for use in docs/monaco
+
+import {
+	filesRecursive,
+	isDirectory,
+	read,
+	readdir,
+	watch,
+	write,
+} from './utils.js'
+
+// continuously watch
+
+let timeout
+
+watch('./', () => {
+	clearTimeout(timeout)
+	timeout = setTimeout(run, 5000)
+})
+
+process.on('exit', run) // run on exit
+
+// importmap
+
+function run() {
+	{
+		const plugin = readdir('./src/plugin')
+			.filter(x => x.endsWith('.js'))
+			.map(
+				x =>
+					`"pota/plugin/${x.replace(/\.js$/, '')}": "/node_modules/pota/src/plugin/${x}"`,
+			)
+
+		const lib = readdir('./src/lib')
+			.filter(x => x.endsWith('.js') && !x.endsWith('solid.js'))
+			.map(
+				x =>
+					`"pota/${x.replace(/.js$/, '')}": "/node_modules/pota/src/lib/${x}"`,
+			)
+
+		write(
+			'./src/release/importmap.json',
+			`{ "imports": {
+
+"pota": "/node_modules/pota/src/exports.js",
+
+"pota/babel-preset": "/node_modules/pota/src/babel-preset/index.js",
+"pota/jsx-runtime": "/node_modules/pota/src/jsx/jsx-runtime.js",
+"pota/jsx-dev-runtime": "/node_modules/pota/src/jsx/jsx-runtime.js",
+
+"pota/xml": "/node_modules/pota/src/core/xml.js",
+"pota/components": "/node_modules/pota/src/components/@main.js",
+
+${plugin.join(',\n')},
+
+${lib.join(',\n')}
+
+}}`,
+		)
+	}
+
+	// types
+
+	{
+		const types = []
+
+		const files = filesRecursive('./')
+
+		for (const file of files) {
+			if (
+				!isDirectory(file) &&
+				!file.includes('node_modules') &&
+				file.includes('.d.ts') &&
+				!file.includes('.d.ts.map')
+			) {
+				types.push({
+					f: file.replace(/\\/g, '/').replace(/^\.\//, 'pota/'),
+					c: read(file),
+				})
+			}
+		}
+
+		const importmap = JSON.parse(read('./src/release/importmap.json'))
+		for (const moduleName in importmap.imports) {
+			if (moduleName === 'pota') {
+				types.push({
+					f: moduleName + '/index.d.ts',
+					c: `export * from "${importmap.imports[moduleName].replace(/^.*\/pota\/src\//, 'pota/types/').replace(/\.js$/, '.d.ts')}"
+						export * from "pota/pota.d.ts"`,
+				})
+			} else if (
+				moduleName === 'pota/jsx-runtime' ||
+				moduleName === 'pota/jsx-dev-runtime'
+			) {
+				types.push({
+					f: moduleName + '/index.d.ts',
+					c: `export * from "pota/pota.d.ts"`,
+				})
+			} else {
+				types.push({
+					f: moduleName + '/index.d.ts',
+					c: `export * from "${importmap.imports[moduleName].replace(/^.*\/pota\/src\//, 'pota/types/').replace(/\.js$/, '.d.ts')}"`,
+				})
+			}
+		}
+
+		write('./src/release/types.json', JSON.stringify(types))
+	}
+
+	console.log('Generated importmap.json and types.json')
+}
---
+++ src/release/utils.js
@@ -0,0 +1,58 @@
+import path from 'path'
+import fs from 'fs'
+
+export const read = name =>
+	fs.readFileSync(name, { encoding: 'utf8' })
+
+export const write = (name, content) => {
+	if (!exists(name) || read(name) !== content) {
+		fs.writeFileSync(mkdir(name), content)
+	}
+}
+
+export const exists = name => fs.existsSync(name)
+
+export const append = (name, content) =>
+	fs.appendFileSync(mkdir(name), content)
+
+export const isDirectory = name => fs.statSync(name).isDirectory()
+
+export const remove = name => {
+	try {
+		fs.rmSync(name, { recursive: true })
+		if (/[^\/]+\.[^\/]+$/.test(name)) {
+			fs.rmdirSync(path.dirname(name))
+		}
+	} catch (e) {}
+}
+
+export const move = (source, destination) =>
+	fs.renameSync(source, mkdir(destination))
+
+export const copy = (source, destination) =>
+	fs.copyFileSync(source, mkdir(destination))
+
+export const mkdir = dir => {
+	fs.mkdirSync(
+		/[^\/]+\.[^\/]+$/.test(dir) ? path.dirname(dir) : dir,
+		{
+			recursive: true,
+		},
+	)
+	return dir
+}
+
+export const files = dir =>
+	fs.readdirSync(dir).map(file => dir + file)
+
+export const readdir = dir => fs.readdirSync(dir)
+
+export const filesRecursive = dir =>
+	fs
+		.readdirSync(dir, {
+			recursive: true,
+		})
+		.map(file => dir + file)
+
+export const watch = (dir, fn) =>
+	fs.watch(dir, { recursive: true }, fn)
