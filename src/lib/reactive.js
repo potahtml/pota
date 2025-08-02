727fa8821f6a67af08cf39857f810ab309f92b1e
Tito Bouzout <tito.bouzout@gmail.com>
2025-08-02 18:17:28 -0300

    remove `proxy` utility, not needed

--- src/lib/reactive.js
+++ src/lib/reactive.js
@@ -58,29 +58,6 @@ export {
 }
 
 /**
- * Proxies a signals property access so you dont have to call the
- * function
- *
- * @template {{}} T
- * @param {SignalAccessor<T>} snigal - Signal to proxy
- * @param {object} [target] - Target object for the proxy
- * @returns {object} An object that will read the properties from the
- *   signal
- */
-export const proxy = (snigal, target = nothing) =>
-	new Proxy(target, {
-		get(target, key) {
-			return snigal()[key]
-		},
-		has(target, key) {
-			return key in snigal()
-		},
-		ownKeys(target) {
-			return keys(snigal())
-		},
-	})
-
-/**
  * A self contained signal function, when an argument is present it
  * writes to the signal, when theres no argument it reads the signal.
  *
