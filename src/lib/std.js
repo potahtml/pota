4d2a0ae5d107eda2ac5990df8d999ed37c77702c
Tito Bouzout <tito.bouzout@gmail.com>
2025-08-04 04:22:22 -0300

    remove unused code

--- src/lib/std.js
+++ src/lib/std.js
@@ -556,22 +556,6 @@ export function indexByKey(iterable, key) {
 	return byKey
 }
 
-/**
- * Replace a prototype in the prototype chain with another prototype
- *
- * @param {object} target - Target object
- * @param {object} search - The prototype to replace
- * @param {object} replacement - The replacement prototype
- */
-export function replacePrototypeWith(target, search, replacement) {
-	let prototype = target
-	while (getPrototypeOf(prototype) !== search) {
-		prototype = getPrototypeOf(prototype)
-	}
-
-	setPrototypeOf(prototype, replacement)
-}
-
 export const typeString = obj =>
 	Object.prototype.toString.call(obj).slice(8, -1)
 
