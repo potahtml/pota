0e9665bb33a18ea2c9c15a60698c2a053812eb20
Tito Bouzout <tito.bouzout@gmail.com>
2025-12-10 16:04:29 -0300

    typescript drives me crazy, cant figure out the proper setup

--- typescript/exports.d.ts
+++ typescript/exports.d.ts
@@ -1,3 +1,3 @@
-export * from './pota.d.ts'
-export * from '../src/jsx/jsx.d.ts'
-export * from '../types/exports.d.ts'
+export type * from './pota.d.ts'
+export type * from '../src/jsx/jsx.d.ts'
+export type * from '../types/exports.d.ts'
--- typescript/pota.d.ts
+++ typescript/pota.d.ts
@@ -1,155 +1,160 @@
 import type { JSX } from '../src/jsx/jsx.d.ts'
 
-// accessor
-export type Accessor<T> = (() => Accessor<T>) | SignalAccessor<T> | T
+declare global {
+	// accessor
+	export type Accessor<T> =
+		| (() => Accessor<T>)
+		| SignalAccessor<T>
+		| T
 
-export type Accessed<T> =
-	T extends PromiseLike<infer R>
-		? Accessed<R>
-		: T extends Derived<infer R>
+	export type Accessed<T> =
+		T extends PromiseLike<infer R>
 			? Accessed<R>
-			: T extends SignalAccessor<infer R>
+			: T extends Derived<infer R>
 				? Accessed<R>
-				: T extends SignalFunction<infer R>
+				: T extends SignalAccessor<infer R>
 					? Accessed<R>
-					: T extends { (): infer R }
+					: T extends SignalFunction<infer R>
 						? Accessed<R>
-						: T
+						: T extends { (): infer R }
+							? Accessed<R>
+							: T
 
-// signal
+	// signal
 
-export type SignalAccessor<T> = () => T
+	export type SignalAccessor<T> = () => T
 
-export type SignalSetter<T> = (newValue?: T) => SignalChanged
+	export type SignalSetter<T> = (newValue?: T) => SignalChanged
 
-export type SignalUpdate<T> = (
-	updateFunction: (prevValue: T) => T,
-) => SignalChanged
+	export type SignalUpdate<T> = (
+		updateFunction: (prevValue: T) => T,
+	) => SignalChanged
 
-// signal as object/function/tuple
+	// signal as object/function/tuple
 
-export type SignalTuple<T> = readonly [
-	SignalAccessor<T>,
-	SignalSetter<T>,
-	SignalUpdate<T>,
-]
+	export type SignalTuple<T> = readonly [
+		SignalAccessor<T>,
+		SignalSetter<T>,
+		SignalUpdate<T>,
+	]
 
-export type SignalObject<T> = SignalTuple<T> & {
-	readonly read: SignalAccessor<T>
-	readonly write: SignalSetter<T>
-	readonly update: SignalUpdate<T>
-}
+	export type SignalObject<T> = SignalTuple<T> & {
+		readonly read: SignalAccessor<T>
+		readonly write: SignalSetter<T>
+		readonly update: SignalUpdate<T>
+	}
 
-interface SignalFunction<T> {
-	(): T
-	(value: T): SignalChanged
-}
+	interface SignalFunction<T> {
+		(): T
+		(value: T): SignalChanged
+	}
 
-// signal properties
+	// signal properties
 
-export type SignalOptions<T> =
-	| {
-			equals?: false | ((a: T, b: T) => boolean)
-	  }
-	| undefined
+	export type SignalOptions<T> =
+		| {
+				equals?: false | ((a: T, b: T) => boolean)
+		  }
+		| undefined
 
-export type SignalChanged = boolean
+	export type SignalChanged = boolean
 
-// derived
+	// derived
 
-export type DerivedSignal<R> = {
-	(): R // getter
-	(value: R): SignalChanged // setter
-	run: Function
-	resolved: SignalAccessor<boolean>
-}
+	export type DerivedSignal<R> = {
+		(): R // getter
+		(value: R): SignalChanged // setter
+		run: Function
+		resolved: SignalAccessor<boolean>
+	}
 
-export type Derived<R> = DerivedSignal<R> &
-	PromiseLike<DerivedSignal<R>>
+	export type Derived<R> = DerivedSignal<R> &
+		PromiseLike<DerivedSignal<R>>
 
-// props
+	// props
 
-export type When<T> = Accessor<T>
-export type Each<T> = Accessor<Iterable<T>>
+	export type When<T> = Accessor<T>
+	export type Each<T> = Accessor<Iterable<T>>
 
-// components
+	// components
 
-export type Component = JSX.ElementType
-export type Children = JSX.Element
-export type Elements = JSX.Elements
+	export type Component = JSX.ElementType
+	export type Children = JSX.Element
+	export type Elements = JSX.Elements
 
-export type Props<T = {}> = JSX.Props<T>
+	export type Props<T = {}> = JSX.Props<T>
 
-// based on solidjs types
-export type ComponentProps<T> = T extends (props: infer P) => any
-	? P
-	: T extends keyof JSX.IntrinsicElements
-		? JSX.IntrinsicElements[T]
-		: Record<string, unknown>
+	// based on solidjs types
+	export type ComponentProps<T> = T extends (props: infer P) => any
+		? P
+		: T extends keyof JSX.IntrinsicElements
+			? JSX.IntrinsicElements[T]
+			: Record<string, unknown>
 
-export type Dynamic<P extends { component: any }> = ComponentProps<
-	P['component']
-> & {
-	component: P['component']
-}
+	export type Dynamic<P extends { component: any }> = ComponentProps<
+		P['component']
+	> & {
+		component: P['component']
+	}
 
-// dom
+	// dom
 
-export type DOMElement = JSX.DOMElement
+	export type DOMElement = JSX.DOMElement
 
-export type StyleAttribute = JSX.StyleAttribute
+	export type StyleAttribute = JSX.StyleAttribute
 
-// events
+	// events
 
-// string types of events
-export type EventName = JSX.EventName
-export type EventType = JSX.EventType
+	// string types of events
+	export type EventName = JSX.EventName
+	export type EventType = JSX.EventType
 
-// actual possible events
-export type EventHandlersWindow<Element> =
-	JSX.EventHandlersWindow<Element>
-export type EventHandlersElement<Element> =
-	JSX.EventHandlersElement<Element>
+	// actual possible events
+	export type EventHandlersWindow<Element> =
+		JSX.EventHandlersWindow<Element>
+	export type EventHandlersElement<Element> =
+		JSX.EventHandlersElement<Element>
 
-// event options
-export type EventHandlerOptions = JSX.EventHandlerOptions
+	// event options
+	export type EventHandlerOptions = JSX.EventHandlerOptions
 
-// a generic event handler
-export type EventFunction<Event, Element> = JSX.EventFunction<
-	Event,
-	Element
->
-export type EventEvent<Event, Element> = JSX.EventEvent<
-	Event,
-	Element
->
-export type EventObject<Event, Element> = JSX.EventObject<
-	Event,
-	Element
->
-export type EventHandler<Event, Element> = JSX.EventHandler<
-	Event,
-	Element
->
-export type EventHandlers<Event, Element> = JSX.EventHandlers<
-	Event,
-	Element
->
+	// a generic event handler
+	export type EventFunction<Event, Element> = JSX.EventFunction<
+		Event,
+		Element
+	>
+	export type EventEvent<Event, Element> = JSX.EventEvent<
+		Event,
+		Element
+	>
+	export type EventObject<Event, Element> = JSX.EventObject<
+		Event,
+		Element
+	>
+	export type EventHandler<Event, Element> = JSX.EventHandler<
+		Event,
+		Element
+	>
+	export type EventHandlers<Event, Element> = JSX.EventHandlers<
+		Event,
+		Element
+	>
 
-// callbacks
+	// callbacks
 
-export type CallbackElement<Element> = JSX.CallbackElement<Element>
-export type CallbackEvent<Event> = JSX.CallbackEvent<Event>
-export type CallbackEventElement<Event, Element> =
-	JSX.CallbackEventElement<Event, Element>
+	export type CallbackElement<Element> = JSX.CallbackElement<Element>
+	export type CallbackEvent<Event> = JSX.CallbackEvent<Event>
+	export type CallbackEventElement<Event, Element> =
+		JSX.CallbackEventElement<Event, Element>
 
-// tests
+	// tests
 
-export type Expect = {
-	toBe: (expected: unknown) => Promise<unknown>
-	toEqual: (expected: unknown) => Promise<unknown>
-	not: {
+	export type Expect = {
 		toBe: (expected: unknown) => Promise<unknown>
 		toEqual: (expected: unknown) => Promise<unknown>
+		not: {
+			toBe: (expected: unknown) => Promise<unknown>
+			toEqual: (expected: unknown) => Promise<unknown>
+		}
 	}
 }
