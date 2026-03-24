ceb733f6b17d9f05a9ae699f034d767afdcc2139
Anthony K. <kilospade@gmail.com>
2026-03-23 23:01:05 -0500

    Add reactivity to Range component's props

--- src/components/Range.js
+++ src/components/Range.js
@@ -1,4 +1,4 @@
-import { range, toArray } from '../lib/std.js'
+import { getValue, range, toArray } from '../lib/std.js'
 import { Component } from '../core/renderer.js'
 import { For } from './For.js'
 
@@ -6,17 +6,22 @@ import { For } from './For.js'
  * Renders children based on the `range` function arguments
  *
  * @param {object} props
- * @param {number} [props.start]
- * @param {number} [props.stop]
- * @param {number} [props.step]
+ * @param {Accessor<number>} [props.start]
+ * @param {Accessor<number>} [props.stop]
+ * @param {Accessor<number>} [props.step]
  * @param {Children} [props.children]
  * @returns {Children}
  */
 
 export const Range = props =>
 	Component(For, {
-		each: toArray(
-			range(props.start ?? 0, props.stop ?? 0, props.step ?? 1),
-		),
+		each: () =>
+			toArray(
+				range(
+					getValue(props.start) ?? 0,
+					getValue(props.stop) ?? 0,
+					getValue(props.step) ?? 1,
+				),
+			),
 		children: props.children,
 	})
